b38aea1b569d8b603d48d229fb1aa6aaada4adef
xuchaochi <120047898@qq.com>
2018-11-25 02:30:59 +0800

    完成消息队列逻辑

--- src/common/utility/XMsgQueue.hpp
+++ src/common/utility/XMsgQueue.hpp
@@ -3,38 +3,85 @@
 #include "utility/XNoCopyable.hpp"
 #include <condition_variable>
 #include <vector>
+#include <mutex>
+#include <atomic>
 
 XSVR_NS_BEGIN
 template <typename TMsg>
 class XMsgQueue
 {
     XSVR_NOCOPYABLE(XMsgQueue)
-  public:   
+  public:
+    XMsgQueue(uint32_t nSize)
+        : m_nFrontIndex(0),
+          m_nTailIndex(0)
+    {
+        m_queue.resize(nSize);
+    }
     bool isFull() const
     {
-        return true;
+        std::lock_guard<std::mutex> lck(m_mutex);
+        return _isFull();
     }
     bool isEmpty() const
     {
-        return true;
+        std::lock_guard<std::mutex> lck(m_mutex);
+        return _isEmpty();
     }
     bool push(const TMsg &msg)
     {
+        std::unique_lock<std::mutex> lck(m_mutex);
+        if (_isFull())
+        {
+            return false;
+        }
+        else
+        {
+            m_queue[m_nTailIndex] = msg;
+            m_nTailIndex = ++m_nTailIndex % m_queue.capacity();
+            m_emptyCv.notify_one();
+        }
         return true;
     }
-    bool pop(TMsg &msg)
+    bool get(TMsg &msg)
     {
+        std::unique_lock<std::mutex> lck(m_mutex);
+        while (_isEmpty())
+        {
+            m_emptyCv.wait(lck);
+        }
+        msg = m_queue[m_nFrontIndex];
+        m_nFrontIndex = ++m_nTailIndex % m_queue.capacity();
+        m_emptyCv.notify_one();
         return true;
     }
-    size_t size() const
+    uint32_t size()
     {
-        return 0;
+        std::lock_guard<std::mutex> lck(m_mutex);
+        return (m_nTailIndex - m_nFrontIndex + m_queue.capacity()) % m_queue.capacity();
     }
-    private:
+
+  private:
+    bool _isFull() const
+    {
+        uint32_t nTempFront = m_nFrontIndex % m_queue.capacity();
+        uint32_t nTempTail = (m_nTailIndex + 1) % m_queue.capacity();
+        if (nTempFront == nTempTail)
+        {
+            return true;
+        }
+        return false;
+    }
+    bool _isEmpty() const
+    {
+        return m_nFrontIndex == m_nTailIndex;
+    }
+
     std::vector<TMsg> m_queue;
     std::condition_variable m_emptyCv;
-    std::condition_variable m_fullCv;
     std::mutex m_mutex;
+    uint32_t m_nFrontIndex;
+    uint32_t m_nTailIndex;
 };
 
 XSVR_NS_END
