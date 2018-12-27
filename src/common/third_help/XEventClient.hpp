162c83ea7d4a4acf9385ec39faece6dcf8548f78
xuchaochi <120047898@qq.com>
2018-12-28 01:00:09 +0800

    初步封装

--- src/common/third_help/XEventClient.hpp
+++ src/common/third_help/XEventClient.hpp
@@ -1,3 +1,6 @@
+//libevent客户端封装
+//考虑到是服务端间的通行开了线程，多客户端性能不是很好。
+//如果需要多客户端可以把event_base_dispatch和event_base提取出来
 #pragma once
 extern "C"
 {
@@ -8,40 +11,145 @@ extern "C"
 #include "event2/buffer.h"
 #include "event2/util.h"
 }
+#include <netinet/in.h>
+#include <sys/socket.h>
 #include <string>
 #include <memory>
 #include "base/XBase.h"
-//#include "third_help/XEventServer.hpp"
+#define SOCKET_HEADER_LENGTH sizeof(uint32_t)
 XSVR_NS_BEGIN
 class XEventServer;
 class XEventClient
 {
+  friend XEventServer;
+
 public:
-  XEventClient() {}
-  ~XEventClient() {}
+  XEventClient()
+      : m_bConnected(false) {}
+  ~XEventClient()
+  {
+    if (m_bConnected.load())
+    {
+      close();
+    }
+  }
   bool connnectToHost(const std::string &strIP, uint16_t nPort)
   {
-    return true;
+    bool bRet = false;
+    do
+    {
+      if (m_bConnected.load())
+      {
+        onError("SOCKET ALREADY CONNECTED");
+        break;
+      }
+      m_pEvBase = std::shared_ptr<event_base>(event_base_new(), [](event_base *pEvBase) {
+        if (pEvBase)
+        {
+          event_base_free(pEvBase);
+        }
+      });
+      if (!m_pEvBase)
+      {
+        onError("event_base_new");
+        break;
+      }
+
+      m_stAddr.sin_family = AF_INET;
+      m_stAddr.sin_port = htons(nPort);
+      m_stAddr.sin_addr.s_addr = strIP.empty() ? 0 : inet_addr(strIP.c_str());
+      m_pBufferEv = std::shared_ptr<bufferevent>(bufferevent_socket_new(m_pEvBase.get(), -1, BEV_OPT_CLOSE_ON_FREE | BEV_OPT_THREADSAFE),
+                                                 [](bufferevent *pEvBuf) {
+                                                   if (pEvBuf)
+                                                   {
+                                                     bufferevent_free(pEvBuf);
+                                                   }
+                                                 });
+      if (!m_pBufferEv)
+      {
+        onError("bufferevent_socket_new");
+        break;
+      }
+      if (0 != bufferevent_socket_connect(m_pBufferEv.get(), (struct sockaddr *)&m_stAddr, sizeof(m_stAddr)))
+      {
+        onError("bufferevent_socket_connect");
+        break;
+      }
+      bufferevent_setcb(m_pBufferEv.get(), readCallback, NULL, eventCallback, (void *)this);
+      if (0 != bufferevent_enable(m_pBufferEv.get(), EV_READ | EV_PERSIST))
+      {
+        onError("bufferevent_enable");
+        break;
+      }
+      if (0 != evutil_make_socket_nonblocking(bufferevent_getfd(m_pBufferEv.get())))
+      {
+        onError("evutil_make_socket_nonblocking");
+        break;
+      }
+      m_pThread = std::make_shared<std::thread>([this]() {
+        if (0 != event_base_dispatch(m_pEvBase.get()))
+        {
+          onError("event_base_dispatch");
+        }
+        int a = 10;
+      });
+      bRet = true;
+    } while (0);
+    return bRet;
   }
 
   bool close()
   {
+    if (!m_bConnected.load())
+    {
+      onError("ID(" + std::to_string(id()) + "):NOT CONNECTED, CANT CLOSE");
+      return false;
+    }
+    else
+    {
+      if (m_pEvBase)
+      {
+        m_pBufferEv = nullptr;
+        if (0 != event_base_loopbreak(m_pEvBase.get()))
+        {
+          onError("event_base_loopbreak");
+          return false;
+        }
+        m_pEvBase = nullptr;
+      }
+      if (m_pThread)
+      {
+        m_pThread->join();
+      }
+      m_bConnected.store(false);
+    }
+
     return true;
   }
 
   int32_t send(const std::string &strData)
   {
-    return 0;
+    if (!m_bConnected.load())
+    {
+      onError("SOCKET NOT CONNECTED");
+      return -1;
+    }
+    if (0 != bufferevent_write(m_pBufferEv.get(), strData.c_str(), strData.size()))
+    {
+      onError("bufferevent_write");
+      return -1;
+    }
+    return strData.size();
   }
 
   std::string ip() const
   {
-    return "";
+    return inet_ntoa(m_stAddr.sin_addr);
   }
 
   uint16_t port() const
   {
-    return 0;
+    return htons(m_stAddr.sin_port);
   }
 
   uint32_t id() const
@@ -49,10 +157,98 @@ public:
     return m_nID;
   }
 
-  friend XEventServer;
-
 private:
-  std::shared_ptr<bufferevent> m_pBufferEv;
+  static void readCallback(bufferevent *pBEv, void *pArg)
+  {
+    auto pClient = static_cast<XEventClient *>(pArg);
+    if (pClient)
+    {
+      auto pInputBuf = pBEv->input;
+      const char *pHeader = nullptr;
+      uint32_t nPacketLen = 0;
+      pHeader = (const char *)evbuffer_pullup(pInputBuf, -1);
+      uint32_t nCurLen = evbuffer_get_length(pInputBuf);
+      pClient->onLog("cursize:" + std::to_string(nCurLen));
+      if (pHeader && nCurLen >= SOCKET_HEADER_LENGTH)
+      {
+        memcpy(&nPacketLen, pHeader, SOCKET_HEADER_LENGTH);
+        pClient->onLog("packetsize:" + std::to_string(nPacketLen));
+        if (nPacketLen < 0)
+        {
+          pClient->onError("packetsize < 0");
+          pClient->close();
+        }
+        if (nCurLen >= nPacketLen)
+        {
+          if (pClient->m_pReadFunc)
+          {
+            pClient->m_pReadFunc(pHeader + SOCKET_HEADER_LENGTH, nPacketLen - SOCKET_HEADER_LENGTH);
+            if (0 != evbuffer_drain(pInputBuf, nPacketLen))
+            {
+              pClient->onError("evbuffer_drain");
+              pClient->close();
+            }
+          }
+        }
+      }
+    }
+  }
+
+  static void eventCallback(bufferevent *pBufferEv, short nEvent, void *pArg)
+  {
+    auto pClient = static_cast<XEventClient *>(pArg);
+    if (nEvent & BEV_EVENT_EOF)
+    {
+      pClient->onError("BEV_EVENT_EOF");
+    }
+    else if (nEvent & BEV_EVENT_ERROR)
+    {
+      auto err = EVUTIL_SOCKET_ERROR();
+      pClient->onError(evutil_socket_error_to_string(err));
+    }
+    else if ((nEvent & BEV_EVENT_TIMEOUT))
+    {
+      pClient->onLog("BEV_EVENT_TIMEOUT");
+    }
+    else if ((nEvent & BEV_EVENT_CONNECTED))
+    {
+      pClient->onConnected();
+      pClient->onLog("BEV_EVENT_CONNECTED");
+    }
+  }
+
+  void onLog(const std::string &strMsg)
+  {
+    if (m_pLogFunc)
+    {
+      m_pLogFunc(std::move(strMsg));
+    }
+  }
+  void onError(const std::string &strMsg)
+  {
+    if (m_pErrorFunc)
+    {
+      m_pErrorFunc(std::move(strMsg));
+    }
+  }
+  void onConnected()
+  {
+    m_bConnected.store(true);
+    if (m_pConnectedFunc)
+    {
+      m_pConnectedFunc();
+    }
+  }
+  std::function<void(const char *pBuf, uint32_t nLen)> m_pReadFunc = nullptr;
+  std::function<void(const std::string &)> m_pErrorFunc = nullptr;
+  std::function<void(const std::string &)> m_pLogFunc = nullptr;
+  std::function<void()> m_pConnectedFunc = nullptr;
+
+  std::shared_ptr<bufferevent> m_pBufferEv = nullptr;
+  std::shared_ptr<event_base> m_pEvBase = nullptr;
+  std::atomic_bool m_bConnected;
   uint32_t m_nID = 0;
+  sockaddr_in m_stAddr;
+  std::shared_ptr<std::thread> m_pThread = nullptr;
 };
 XSVR_NS_END
--- src/common/third_help/XEventServer.hpp
+++ src/common/third_help/XEventServer.hpp
@@ -2,6 +2,7 @@
 extern "C"
 {
 #include "event.h"
+#include "event2/event_struct.h"
 #include "event2/listener.h"
 #include "event2/bufferevent.h"
 #include "event2/thread.h"
@@ -23,7 +24,7 @@ extern "C"
 #include "utility/XNoCopyable.hpp"
 #include "third_help/XEventClient.hpp"
 XSVR_NS_BEGIN
-#define SOCKET_HEADER_LENGTH sizeof(uint32_t)
+
 using XEvClientPtr = std::shared_ptr<XEventClient>;
 using BufferEvPtr = std::shared_ptr<bufferevent>;
 class XEventServer
@@ -31,50 +32,98 @@ class XEventServer
     XSVR_NOCOPYABLE(XEventServer)
   public:
     XEventServer()
+        : m_bListened(false)
     {
         m_nClientID.store(0);
     }
     ~XEventServer()
     {
+        if (m_bListened.load())
+        {
+            close();
+        }
         onLog("SVR EXIT.....");
     }
     bool listen(std::string strIp, uint16_t nPort)
     {
-        sockaddr_in stAddr;
-        memset(&stAddr, 0, sizeof(sockaddr_in));
-        stAddr.sin_family = AF_INET;
-        stAddr.sin_port = htons(nPort);
-        stAddr.sin_addr.s_addr = strIp.empty() ? 0 : inet_addr(strIp.c_str());
-
-        m_pEventBase = std::shared_ptr<event_base>(::event_base_new(), [](event_base *pBase) {
-            event_base_free(pBase);
-        });
-
-        auto pListener = std::shared_ptr<evconnlistener>(evconnlistener_new_bind(
-                                                             m_pEventBase.get(),
-                                                             &listenCallback,
-                                                             this,
-                                                             LEV_OPT_CLOSE_ON_FREE | LEV_OPT_THREADSAFE,
-                                                             -1,
-                                                             (sockaddr *)&stAddr,
-                                                             sizeof(sockaddr_in)),
-                                                         [](evconnlistener *pListener) {
-                                                             if (pListener)
-                                                             {
-                                                                 evconnlistener_free(pListener);
-                                                             }
-                                                         });
-        event_base_dispatch(m_pEventBase.get());
-        // m_pThread = std::make_shared<std::thread>([this]() {
-        //     int nCode  = event_base_dispatch(m_pEventBase.get());
-        //     onLog("loop end:" + std::to_string(nCode));
-        // });
-
+        do
+        {
+            if (m_bListened.load())
+            {
+                onError("SVR ALREADY CONNECTED");
+                break;
+            }
+            sockaddr_in stAddr;
+            memset(&stAddr, 0, sizeof(sockaddr_in));
+            stAddr.sin_family = AF_INET;
+            stAddr.sin_port = htons(nPort);
+            stAddr.sin_addr.s_addr = strIp.empty() ? 0 : inet_addr(strIp.c_str());
+            m_pEventBase = std::shared_ptr<event_base>(::event_base_new(), [](event_base *pBase) {
+                event_base_free(pBase);
+            });
+            if (!m_pEventBase)
+            {
+                onError("event_base_new");
+                break;
+            }
+            m_pListen = std::shared_ptr<evconnlistener>(evconnlistener_new_bind(
+                                                            m_pEventBase.get(),
+                                                            &listenCallback,
+                                                            this,
+                                                            LEV_OPT_CLOSE_ON_FREE | LEV_OPT_THREADSAFE | LEV_OPT_REUSEABLE,
+                                                            -1,
+                                                            (sockaddr *)&stAddr,
+                                                            sizeof(stAddr)),
+                                                        [](evconnlistener *pListener) {
+                                                            if (pListener)
+                                                            {
+                                                                evconnlistener_free(pListener);
+                                                            }
+                                                        });
+            if (!m_pListen)
+            {
+                onError("evconnlistener_new_bind");
+                break;
+            }
+            m_pThread = std::make_shared<std::thread>([this]() {
+                int nCode = event_base_dispatch(m_pEventBase.get());
+            });
+            m_bListened.store(true);
+        } while (0);
+        return m_bListened.load();
+    }
+    bool close()
+    {
+        if (!m_bListened.load())
+        {
+            onError("SVR NOT START, CANT CLOSE");
+            return false;
+        }
+        else
+        {
+            if (m_pEventBase)
+            {
+                m_pListen = nullptr;
+                if (0 != event_base_loopbreak(m_pEventBase.get()))
+                {
+                    onError("event_base_loopbreak");
+                    return false;
+                }
+                m_pEventBase = nullptr;            
+            }
+            if (m_pThread)
+            {
+                m_pThread->join();
+            }
+            delAllClient();  
+            m_bListened.store(false);
+        }
         return true;
     }
-    bool stop() {return true;}
+
     using readFuncPtr = std::function<void(XEvClientPtr, const char *, uint32_t)>;
     using msgFuncPtr = std::function<void(std::string)>;
+
     void setCallback(readFuncPtr readCbFunc,
                      msgFuncPtr errCbFunc,
                      msgFuncPtr logCbFunc)
@@ -95,7 +144,7 @@ class XEventServer
         onError("pthread not define");
 #endif
 #endif
-return true;
+        return true;
     }
 
   private:
@@ -119,11 +168,33 @@ return true;
         pSvr->onLog("new connected");
         std::shared_ptr<XEventClient> pClient = std::make_shared<XEventClient>();
         pClient->m_nID = pSvr->m_nClientID++;
-        evutil_make_socket_nonblocking(nFd);
-        pClient->m_pBufferEv = std::shared_ptr<bufferevent>(bufferevent_socket_new(pSvr->m_pEventBase.get(), nFd, BEV_OPT_CLOSE_ON_FREE));
-        bufferevent_setcb(pClient->m_pBufferEv.get(), readCallback, NULL, eventCallback, pArg);
-        bufferevent_enable(pClient->m_pBufferEv.get(), EV_READ | EV_PERSIST);
-        pSvr->addClient(pClient->m_pBufferEv.get(), pClient);
+        pClient->m_bConnected.store(true);
+        memcpy(&pClient->m_stAddr, pSock, socklen);
+        bool bRet = false;
+        do
+        {
+            if (0 != evutil_make_socket_nonblocking(nFd))
+            {
+                pClient->onError("evutil_make_socket_nonblocking");
+                break;
+            }
+            pClient->m_pBufferEv = std::shared_ptr<bufferevent>(bufferevent_socket_new(pSvr->m_pEventBase.get(), nFd, BEV_OPT_CLOSE_ON_FREE));
+            if (!pClient->m_pBufferEv)
+            {
+                pClient->onError("bufferevent_socket_new");
+                break;
+            }
+            bufferevent_setcb(pClient->m_pBufferEv.get(), readCallback, NULL, eventCallback, pArg);
+            if (0 != bufferevent_enable(pClient->m_pBufferEv.get(), EV_READ | EV_PERSIST))
+            {
+                pClient->onError("bufferevent_enable");
+            }
+            bRet = true;
+        } while (0);
+        if (bRet)
+        {
+            pSvr->addClient(pClient->m_pBufferEv.get(), pClient);
+        }
     }
 
     static void readCallback(bufferevent *pBEv, void *pArg)
@@ -139,19 +210,22 @@ return true;
         {
             memcpy(&nPacketLen, pHeader, SOCKET_HEADER_LENGTH);
             pSvr->onLog("packetsize:" + std::to_string(nPacketLen));
+            auto pClient = pSvr->getClient(pBEv);
             if (nPacketLen < 0)
             {
-                //todo error
+                pClient->onError("packetsize < 0");
+                pSvr->delClient(pBEv);
             }
             if (nCurLen >= nPacketLen)
             {
                 if (pSvr->m_readCbFunc)
                 {
-                    auto pClient = pSvr->getClient(pBEv);
+
                     pSvr->m_readCbFunc(pClient, pHeader + SOCKET_HEADER_LENGTH, nPacketLen - SOCKET_HEADER_LENGTH);
-                    if (-1 == evbuffer_drain(pInputBuf, nPacketLen))
+                    if (0 != evbuffer_drain(pInputBuf, nPacketLen))
                     {
-                        //todo error
+                        pClient->onError("evbuffer_drain");
+                        pSvr->delClient(pBEv);
                     }
                 }
             }
@@ -193,10 +267,20 @@ return true;
         auto mIter = m_mapEvClient.find(pBEv);
         if (mIter != m_mapEvClient.end())
         {
+            mIter->second->close();
             m_mapEvClient.erase(mIter);
             bRet = true;
         }
         return bRet;
+    }
+
+    void delAllClient()
+    {
+        std::lock_guard<std::mutex> lock(m_clientMutex);
+        for (auto &mIter : m_mapEvClient)
+        {
+            mIter.second->close();
+        }
     }
 
     bool delClient(XEvClientPtr pClient)
@@ -207,6 +291,7 @@ return true;
         {
             if (pClient == mIter.second)
             {
+                pClient->close();
                 m_mapEvClient.erase(mIter.first);
                 bRet = true;
                 break;
@@ -230,12 +315,14 @@ return true;
     }
 
     std::atomic_int m_nClientID;
+    std::atomic_bool m_bListened;
     std::mutex m_clientMutex;
     std::map<bufferevent *, XEvClientPtr> m_mapEvClient;
-    std::shared_ptr<event_base> m_pEventBase;
-    readFuncPtr m_readCbFunc;
-    std::function<void(std::string)> m_errCbFunc;
-    std::function<void(std::string)> m_logCbFunc;
+    readFuncPtr m_readCbFunc = nullptr;
+    std::function<void(std::string)> m_errCbFunc = nullptr;
+    std::function<void(std::string)> m_logCbFunc = nullptr;
+    std::shared_ptr<event_base> m_pEventBase = nullptr;
     std::shared_ptr<std::thread> m_pThread = nullptr;
+    std::shared_ptr<evconnlistener> m_pListen = nullptr;
 };
 XSVR_NS_END
